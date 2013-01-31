96ed55ccface0a9c389e0a549cf2d097b279ee7b
Øystein Dale <dale.oystein@gmail.com>
2013-01-31 14:05:02 +0100

    Code for linear interpolation between two colors

--- MoodLamp/MoodLamp.ino
+++ MoodLamp/MoodLamp.ino
@@ -1,12 +1,40 @@
+/* 
+ * Pin numbers for different colors
+ */
 int RED = 10;
 int GREEN = 11;
 int BLUE = 9;
 int WHITE = 6;
+
+/*
+ * Pin numbers for buttons
+ */
 int PAUSEBUTTON = 2;
 int NEXTBUTTON = 4;
+
+/*
+ * Color values
+ */
+
+int colors[][4] = {
+  {255,   0,    0,    0}, // Red
+  {255, 70,    0,    0},  // Orange
+  {255, 255,    0,    0}, // Yellow
+  {0,   255,    0,    0}, // Green
+  {0,   255,  255,    0}, // Cyan
+  {0,     0,  255,    0}, // Blue
+  {255,   0,  150,    0}, // Pink
+};
+
+int colorCount = 7;
+
+int currentColor = 0;
+int nextColor = 0;
+
 int mod[3];
 int power[4];
 int out[] = {RED, GREEN, BLUE, WHITE};
+
 int lastReadPause = LOW;
 int lastReadNext = LOW;
 int mode = 0;
@@ -17,23 +45,26 @@ double offset = 0.0;
 double lastPushTime = 0.0;
 
 void setup() {
-
+  randomSeed(analogRead(0));
+  currentColor = random(0, colorCount - 1);
+  
   for (int i = 0; i < 4; i++) {
     power[i] = 0;
   }
+
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
   pinMode(WHITE, OUTPUT);
   pinMode(PAUSEBUTTON, INPUT);
   pinMode(NEXTBUTTON, INPUT);
-  
+
   Serial.begin(9600);
 }
 
 int getColorForChar(char c) {
   switch(c) {
-    case 'r': 
+    case 'r':
       return 0;
     case 'g':
       return 1;
@@ -52,6 +83,48 @@ int clamp(int val, int min, int max) {
   return val;
 }
 
+long lastChangeTime = -2000;
+int index = 0;
+const int SECONDS = 1000;
+
+long u = 5000; // Constant color time
+long v = 2500; // Interpolation time
+
+boolean isConstant = true;
+int newColor = 0;
+
+void loop() {
+  if (isConstant) {
+    // Write current color
+    for (int i = 0; i < 4; i++) {
+      analogWrite(out[i], colors[currentColor][i]);
+    }
+    
+    if (millis() - lastChangeTime >= u) {
+        lastChangeTime = millis();
+        isConstant = false;
+        
+        do {
+          newColor = random(0, colorCount - 1);
+        } while (newColor == currentColor);
+    }
+  } else { // interpolating two colors
+    float delta = (float)(millis() - lastChangeTime) / (float)v;
+    
+    if (delta >= 1.0f) {
+      isConstant = true;
+      currentColor = newColor;
+      lastChangeTime = millis();
+      delta = 1.0f;
+    }
+    
+    for (int i = 0; i < 4; i++) {
+      int c = ((1.0f - delta) * (float)colors[currentColor][i]) + ((delta) * (float)colors[newColor][i]);
+      analogWrite(out[i], c);
+    }
+  }
+}
+
 /*
 Reads input from the serial monitor (Ctrl-Shift-M) and changes the color accordingly.
 Multiple colors on one line not permitted
@@ -59,8 +132,9 @@ Usage:
 r0
 g128
 b255
-w 100
+w100
 */
+/*
 void loop() {
   // read 2 bytes at a time
   if (Serial.available() == 2) {
@@ -80,6 +154,7 @@ void loop() {
       analogWrite(out[i], power[i]);
   }
 }
+*/
 
 /*
 void loop() {
@@ -157,4 +232,3 @@ void loop() {
   delay(2);
 }
 */
-
